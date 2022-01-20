14906a0874c8e82dabe32e8a2e8ecce92c14a568
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-01-20 16:05:16 +0100

    UploadFiles

---
+++ index.js
@@ -0,0 +1,243 @@
+// INIZIALIZZA IL BOT
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    { intents:["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGES"] }
+)
+
+client.login("OTMzMzUyMjgxNDgxNTcyMzUy.YegSDA.CqAqNQmC0h7vVZ8AdQOrVWebY58")
+
+//BAN
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!ban")) {
+        var utente = message.mentions.members.first();
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+        if (!utente.bannable) {
+            return message.channel.send('Io non ho il permesso');
+        }
+        utente.ban()
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} bannato`)
+                    .setDescription(`Utente bannato da ${message.author.toString()}`)
+
+                message.channel.send({ embeds: [embed] })
+            })
+    }
+})
+
+//FILTRO PAROLACCE
+client.on("messageCreate", message => {
+    if (message.channel.type == "DM") return
+
+    if (message.member.roles.cache.has("933352810895007844") || message.member.roles.cache.has("933352811800969236")) return
+
+    var parolacce = ["cazzo", "porco dio", "porcodio"]
+    var trovata = false;
+    var testo = message.content;
+
+    parolacce.forEach(parola => {
+        if (message.content.toLowerCase().includes(parola.toLowerCase())) {
+            trovata = true;
+            testo = testo.replace(eval(`/${parola}/g`), "###");
+        }
+    })
+
+    if (trovata) {
+        message.delete();
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Hai detto una parolaccia")
+            .setDescription("Hai scritto un messaggio con parole bloccate\rIl tuo messaggio: " + testo)
+
+        message.channel.send({ embeds: [embed] })
+    }
+})
+
+// SERVER INFO
+client.on("messageCreate", message => {
+    if (message.content == "!serverinfo") {
+        var server = message.member.guild;
+        var embed = new Discord.MessageEmbed()
+            .setTitle(server.name)
+            .setDescription("Tutte le info su questo server")
+            .setThumbnail(server.iconURL())
+            .addField("Owner", client.users.cache.get(server.ownerId).username, true)
+            .addField("Server id", server.id, true)
+            .addField("Members", server.memberCount.toString(), false)
+            .addField("Channels", server.channels.cache.size.toString(), false)
+            .addField("Server created", server.createdAt.toDateString(), true)
+            .addField("Boost level", "Level " + (server.premiumTier != "NONE" ? server.premiumTier : 0) + " (Boost: " + server.premiumSubscriptionCount + ")", true)
+        message.channel.send({ embeds: [embed] })
+    }
+})
+
+// ROLE INFO
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!roleinfo")) {
+        var ruolo = message.mentions.roles.first()
+        if (!ruolo) {
+            return message.channel.send("Non ho trovato questo ruolo")
+        }
+        var memberCount = message.guild.members.cache.filter(member => member.roles.cache.find(role => role == ruolo)).size;
+        var permessiRuolo = new Discord.Permissions(ruolo.permissions.bitfield);
+        var elencoPermessi = "";
+        if (permessiRuolo.has("ADMINISTRATOR")) {
+            elencoPermessi = "ADMINISTRATOR";
+        }
+        else {
+            var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS"]
+            for (var i = 0; i < permessi.length; i++) {
+                if (permessiRuolo.has(permessi[i])) {
+                    elencoPermessi += `- ${permessi[i]}\r`
+                }
+            }
+        }
+        var embed = new Discord.MessageEmbed()
+            .setTitle(ruolo.name)
+            .setDescription("Tutte le statistiche di questo ruolo")
+            .addField("Role ID", ruolo.id, true)
+            .addField("Members", memberCount.toString(), true)
+            .addField("Color", ruolo.hexColor, true)
+            .addField("Role created", ruolo.createdAt.toDateString(), true)
+            .addField("Permissions", elencoPermessi, false)
+        message.channel.send({ embeds: [embed] })
+    }
+})
+
+// STATO
+client.on('ready', () => {
+    //Stato classico (Sta guardando..., Sta giocando a...)
+    client.user.setActivity('Nuovo Bot!', { type: 'WATCHING' }); //Oppure LISTENING, PLAYING
+})
+
+// CLEAR
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!clear")) {
+        if (!message.member.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non ho il permesso');
+        }
+        var count = parseInt(message.content.split(/\s+/)[1]);
+        if (!count) {
+            return message.channel.send("Inserisci un numero valido")
+        }
+        if (count > 100) {
+            return message.channel.send("Non puoi cancellare più di 100 messaggi")
+        }
+        message.channel.bulkDelete(count, true)
+        message.channel.send(count + " messaggi eliminati").then(msg => {
+            msg.delete({ timeout: 5000 })
+        })
+    }
+})
+
+// MUTE
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!mute")) {
+        var utente = message.mentions.members.first();
+        if (!message.member.permissions.has("MANAGE_ROLES")) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+
+        utente.roles.add("896396962113421392+")
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle(`${utente.user.username} mutato`)
+            .setDescription(`Utente mutato da ${message.author.toString()}`)
+
+        message.channel.send({ embeds: [embed] })
+    }
+})
+
+// LOCKDOWN
+var lockdownAttivato = false;
+client.on("messageCreate", message => {
+    if (message.content == "!lockdown") {
+        if (!lockdownAttivato) {
+            message.channel.send("Lockdown ATTIVATO!")
+
+            var everyone = message.guild.roles.everyone
+            everyone.setPermissions(["SEND_MESSAGES", "EMBED_LINKS", "READ_MESSAGE_HISTORY", "USE_VAD"]) //Scrivere tutti i permessi che di default @everyone deve avere tranne VIEW_CHANNEL
+
+            client.channels.cache.get("887061611557294091").permissionOverwrites.edit(everyone, { //Canale in cui è presente un messaggio per avvisare del lockdown attivo (facoltativo)
+                VIEW_CHANNEL: true,
+            })
+        }
+        else {
+            message.channel.send("Lockdown DISATTIVATO!")
+
+            var everyone = message.guild.roles.everyone
+            everyone.setPermissions(["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS", "READ_MESSAGE_HISTORY", "USE_VAD"]) //Scrivere tutti i permessi che di default @everyone
+
+            client.channels.cache.get("887061611557294091").permissionOverwrites.edit(everyone, { //Canale in cui è presente un messaggio per avvisare del lockdown attivo (facoltativo)
+                VIEW_CHANNEL: false,
+            })
+        }
+        lockdownAttivato = !lockdownAttivato
+    }
+})
+
+//KICK
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!kick")) {
+        var utente = message.mentions.members.first();
+        if (!message.member.permissions.has('KICK_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+        if (!utente.kickable) {
+            return message.channel.send('Io non ho il permesso');
+        }
+        utente.kick()
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} kickato`)
+                    .setDescription(`Utente kickato da ${message.author.toString()}`)
+
+                message.channel.send({ embeds: [embed] })
+            })
+    }
+})
+
+//BENVENUTO RUOLO
+client.on("guildMemberAdd", member => {
+    if (member.user.bot) return
+
+    member.roles.add("933384681498505258");
+});
+
+// NOTIFICHE YOUTUBE
+const ytch = require("yt-channel-info") //npm i yt-channel-info
+
+setInterval(() => {
+    ytch.getChannelVideos("UCqFJX8iQDEjfh-qMfB5Urww", "newest").then(async response => {
+        var idVideo = response.items[0]?.videoId
+        if (!idVideo) return
+
+        client.channels.cache.get("887061611557294091").messages.fetch()
+            .then(messages => {
+                var giaMandato = false;
+                messages.forEach(msg => {
+                    if (msg.embeds[0]?.url?.endsWith(idVideo)) giaMandato = true;
+                });
+
+                if (!giaMandato) {
+                    client.channels.cache.get("idCanale").send(`-- NUOVO VIDEO --
+Ciao, è appena uscito un video su **${response.items[0].author}**
+Andate a vedere "${response.items[0].title}"
+https://www.youtu.be/${idVideo}`) //Importate non levare l'id del video
+                }
+            })
+    })
+}, 1000 * 30)
