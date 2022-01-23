be43ab75d1dc611dd0e4474d883271734e2c3cbb
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-01-23 18:49:19 +0100

    Clear

---
+++ commands/moderation/clear.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "clear",
+    aliases: [],
+    onlyStaff: true,
+    description: "Eliminare messaggi in massa",
+    syntax: "!clear [count]",
+    category: "moderation",
+    channelsGranted: [],
+    async execute(message, args, client, property) {
+        try {
+            count = parseInt(message.content.split(/\s+/)[1]) + 1;
+        } catch {
+            return botCommandMessage(message, "Error", "Valore non valido", "Hai inserito un numero non valido", property)
+        }
+
+        if (!count || count < 1) {
+            return botCommandMessage(message, "Error", "Valore non valido", "Hai inserito un numero non valido", property)
+        }
+
+        if (count < 100) {
+            await message.channel.bulkDelete(count, true)
+        }
+        else {
+            await message.channel.bulkDelete(100, true)
+            count = 100
+        }
+    },
+};
--- index.js
+++ index.js
@@ -133,29 +133,6 @@ client.on("messageCreate", message => {
     }
 })
 
-// CLEAR
-client.on("messageCreate", message => {
-    if (message.content.startsWith("!clear")) {
-        if (!message.member.permissions.has("MANAGE_MESSAGES")) {
-            return message.channel.send('Non hai il permesso');
-        }
-        if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) {
-            return message.channel.send('Non ho il permesso');
-        }
-        var count = parseInt(message.content.split(/\s+/)[1]);
-        if (!count) {
-            return message.channel.send("Inserisci un numero valido")
-        }
-        if (count > 100) {
-            return message.channel.send("Non puoi cancellare più di 100 messaggi")
-        }
-        message.channel.bulkDelete(count, true)
-        message.channel.send(count + " messaggi eliminati con successo!").then(msg => {
-            msg.delete({ timeout: 100000 })
-        })
-    }
-})
-
 // MUTE
 client.on("messageCreate", message => {
     if (message.content.startsWith("!mute")) {
