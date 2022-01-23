d647ae28b86a8764db0d32824184069b9f3493ab
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-01-23 18:53:24 +0100

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -1,30 +1,27 @@
-const Discord = require('discord.js');
 
-module.exports = {
-    name: "clear",
-    aliases: [],
-    onlyStaff: true,
-    description: "Eliminare messaggi in massa",
-    syntax: "!clear [count]",
-    category: "moderation",
-    channelsGranted: [],
-    async execute(message, args, client, property) {
-        try {
-            count = parseInt(message.content.split(/\s+/)[1]) + 1;
-        } catch {
-            return botCommandMessage(message, "Error", "Valore non valido", "Hai inserito un numero non valido", property)
-        }
+  module.exports.run = async (bot, message, args) => {
 
-        if (!count || count < 1) {
-            return botCommandMessage(message, "Error", "Valore non valido", "Hai inserito un numero non valido", property)
-        }
 
-        if (count < 100) {
-            await message.channel.bulkDelete(count, true)
-        }
-        else {
-            await message.channel.bulkDelete(100, true)
-            count = 100
-        }
-    },
-};
+    
+    if(message.member.hasPermission("ADMINISTRATOR")) {     ///To allow a simple moderator to use the command just put MANAGE_MESSAGES in place of ADMINISTRATOR 
+      let messagecount = parseInt(args[0]);
+    
+      if(isNaN(messagecount)) return message.channel.send(":x: " + "| Please mention the amount of message that you want to delete");
+    
+      if(messagecount > 100){
+        message.channel.send(":x: " + "| Error, you can only delete between 2 and 100 messages at one time !")
+      }else if(messagecount < 2 ) {
+        message.channel.send(":x: " + "| Error, you can only delete between 2 and 100 messages at one time !")
+      } else {
+    
+      }{
+        message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages, true));
+      }
+    } else {
+      return message.reply(":x: " + "| You need to be \"ADMINISTRATOR\" to do that")
+    }
+    }
+    
+    module.exports.help = {
+        name: "clear"
+    }
