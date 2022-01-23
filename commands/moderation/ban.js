066730ad55cbe3e9f326fc410d2273d3ed2b31d7
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-01-23 19:14:28 +0100

    Ban

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,43 +1,22 @@
-const Discord = require('discord.js');
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!ban")) {
+        var utente = message.mentions.members.first();
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+        if (!utente.bannable) {
+            return message.channel.send('Io non ho il permesso');
+        }
+        utente.ban()
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} bannato`)
+                    .setDescription(`Utente bannato da ${message.author.toString()}`)
 
-module.exports = {
-    name: "ban",
-    description: "Banna membri",
-
-    async run (client, message, args) {
-
-        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('Non hai il permesso!!')
-        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('Non ho il permesso!')
-
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-
-        if(!args[0]) return message.channel.send('Perfavore specifica un utente');
-
-        if(!member) return message.channel.send('Non riesco a trovare questo utente :/');
-        if(!member.bannable) return message.channel.send('Non posso bannarlo!');
-
-        if(member.id === message.author.id) return message.channel.send('Bruh, non posso bannarmi da solo :(');
-
-        let reason = args.slice(1).join(" ");
-
-        if(!reason) reason = 'Unspecified';
-
-        member.ban(`${reason}`).catch(err => { 
-          message.channel.send('Scrivi una ragione')
-            console.log(err)
-        })
-
-        const banembed = new Discord.MessageEmbed()
-        .setTitle('Member Banned')
-        .setThumbnail(member.user.displayAvatarURL())
-        .addField('User Banned', member)
-        .addField('Kicked by', message.author)
-        .addField('Reason', reason)
-        .setFooter('Time kicked', client.user.displayAvatarURL())
-        .setTimestamp()
-
-        message.channel.send(banembed);
-
-
+                message.channel.send({ embeds: [embed] })
+            })
     }
-}
+})
