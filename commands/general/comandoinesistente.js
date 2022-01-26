d702b77cd13d88593fe78c388efb4193d88b666b
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-01-26 17:02:36 +0100

    Update comandoinesistente.js

--- commands/general/comandoinesistente.js
+++ commands/general/comandoinesistente.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js")
 
 client.on("messageCreate", message => {
-    if (message.content == "!") {
+    if (message.content.startsWith("!")) {
+        if (message.content == "!") {
         const embed = new Discord.MessageEmbed()
             .setTitle("🔴 COMANDO NON TROVATO!") //Titolo
             .setDescription("Questo comando non è stato trovato, sei sicuro di averlo scritto bene?") //Descrizione
             .setTimestamp() //Se mettere o no l'orario di arrivo del messaggio
         message.channel.send({embeds: [embed]})
     }
-})
+}})
