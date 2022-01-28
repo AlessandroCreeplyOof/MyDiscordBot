31d266fd9352b8bc305895043fe7aca67d951e12
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-01-29 00:57:21 +0100

    Bug Fixes

--- events/readystatus.js
+++ events/readystatus.js
@@ -5,3 +5,14 @@ client.on('ready', () => {
     client.user.setActivity('Changelog v2322', { type: 'WATCHING' }); //Oppure LISTENING, PLAYING
     client.user.setStatus('online') //Oppure idle, dnd, invisible
 })
+
+setInterval(function () {
+    var canale = client.channels.cache.get("936771474809847908");
+    canale.setName(`👾│members: ${canale.guild.memberCount}`);
+}, 1000 * 60 * 5)
+
+client.on("guildMemberAdd", member => {
+    if (member.user.bot) return
+
+    member.roles.add("933384681498505258");
+});
