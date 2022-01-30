ec78c0767448cd18f979847a957d5b57d3d52214
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-01-30 12:30:53 +0100

    Update readystatus.js

--- events/readystatus.js
+++ events/readystatus.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js")
 
 client.on('ready', () => {
     //Stato classico (Sta guardando..., Sta giocando a...)
-    client.user.setActivity('Changelog v2322', { type: 'WATCHING' }); //Oppure LISTENING, PLAYING
+    client.user.setActivity('Nuovo Logo!', { type: 'WATCHING' }); //Oppure LISTENING, PLAYING
     client.user.setStatus('online') //Oppure idle, dnd, invisible
 })
 
 setInterval(function () {
     var canale = client.channels.cache.get("936771474809847908");
-    canale.setName(`👾│members: ${canale.guild.memberCount}`);
+    canale.setName(`👾 members: ${canale.guild.memberCount}`);
 }, 1000 * 60 * 5)
 
 client.on("guildMemberAdd", member => {
