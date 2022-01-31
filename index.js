0899abd421926c7aac1d9851157e7d3dc73fd5ab
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-01-31 16:55:46 +0100

    Users Log Added

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ client.on("guildMemberAdd", member => {
     if (member.user.bot) return
     var embed = new Discord.MessageEmbed()
         .setTitle("#WELCOME")
-        .setDescription(`${member.toString()} è entrato nel server, Siamo a **${member.guild.memberCount}° membri in totale**`)
+        .setDescription(`${member.user.tag} è entrato nel server, Siamo a **${member.guild.memberCount}° membri in totale**`)
 
     client.channels.cache.get("937720962730819614").send({embeds: [embed]}); 
 })
@@ -67,7 +67,7 @@ client.on("guildMemberRemove", member => {
     if (member.user.bot) return
     var embed = new Discord.MessageEmbed()
         .setTitle("#GOODBYE")
-        .setDescription(`${member.toString()} è uscito dal server`)
+        .setDescription(`${member.user.tag} è uscito dal server`)
 
     client.channels.cache.get("937720962730819614").send({embeds: [embed]}); 
 })
