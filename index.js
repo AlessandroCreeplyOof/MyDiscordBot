cee5b253975dcc1805303f32fc9c11589f4523d4
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-01-31 16:49:13 +0100

    Bug Fixes

--- index.js
+++ index.js
@@ -52,3 +52,22 @@ client.on("messageCreate", message => {
 
     comando.execute(message, args);
 })
+
+//LOG BENVENUTO E ADDIO
+client.on("guildMemberAdd", member => {
+    if (member.user.bot) return
+    var embed = new Discord.MessageEmbed()
+        .setTitle("#WELCOME")
+        .setDescription(`${member.toString()} è entrato nel server, Siamo a **${member.guild.memberCount}° membri in totale**`)
+
+    client.channels.cache.get("937720962730819614").send({embeds: [embed]}); 
+})
+
+client.on("guildMemberRemove", member => {
+    if (member.user.bot) return
+    var embed = new Discord.MessageEmbed()
+        .setTitle("#GOODBYE")
+        .setDescription(`${member.toString()} è uscito dal server`)
+
+    client.channels.cache.get("937720962730819614").send({embeds: [embed]}); 
+})
