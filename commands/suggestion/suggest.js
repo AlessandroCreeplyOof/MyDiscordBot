7bfae30c79a81e9f24582e57eaed80b5b408fdf7
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-02 16:16:54 +0100

    Suggest Custom

---
+++ commands/suggestion/suggest.js
@@ -0,0 +1,44 @@
+module.exports = {
+    name: `suggest`,
+    execute(message, args) {
+        let adminchannel = client.channels.cache.get("936758854593687582")
+        let suggest = args.join(` `)
+        if(!suggest) {
+            let embed = new Discord.MessageEmbed()
+                .setTitle(`:x: Errore`)
+                .setDescription(`Inserisci il suggerimento:\n*\`!suggest [suggerimento]\`*`)
+                .setColor(`RED`)
+            message.reply({embeds: [embed]}).catch(() => { })
+            return
+        }
+        let embed = new Discord.MessageEmbed()
+            .setTitle(`💡Nuovo Suggerimento💡`)
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+            .setColor(`BLUE`)
+            .addField(`⚪Stato:`, `In attesa...`)
+            .addField(`🔮Suggerimento:`, suggest.toString())
+            .addField(`👤Utente:`, `Nome: **${message.author.username}**\nID: **${message.author.id}**\nTag: ${message.author.toString()}`)
+            .setFooter({text: `User ID: ${message.author.id}`})
+        let embed2 = new Discord.MessageEmbed()
+            .setTitle(`💡Nuovo Suggerimento💡`)
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+            .setColor(`BLUE`)
+            .addField(`🔮Suggerimento:`, suggest.toString())
+            .setDescription(`Il tuo suggerimento è stato inviato allo staff.\nAttendi che venga approvato.`)
+        let button1 = new Discord.MessageButton()
+            .setStyle(`SUCCESS`)
+            .setLabel(`Approva`)
+            .setEmoji(`✅`)
+            .setCustomId(`Approva`)
+        let button2 = new Discord.MessageButton()
+            .setStyle(`DANGER`)
+            .setLabel(`Rifiuta`)
+            .setEmoji(`❌`)
+            .setCustomId(`Rifiuta`)
+        let row = new Discord.MessageActionRow()
+            .addComponents(button1)
+            .addComponents(button2)
+        adminchannel.send({embeds: [embed], components: [row]})
+        message.reply({embeds: [embed2]}).catch(() => { })
+    }
+}
--- index.js
+++ index.js
@@ -10,26 +10,56 @@ const fs = require("fs");
 
 client.commands = new Discord.Collection();
 
-const commandsFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+const commandsFiles = fs.readdirSync(`./commands`).filter(file => file.endsWith(`.js`));
 for (const file of commandsFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
-const commandsFolder = fs.readdirSync("./commands");
+const commandsFolder = fs.readdirSync(`./commands`);
 for (const folder of commandsFolder) {
-    const commandsFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith(".js"));
+    const commandsFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith(`.js`));
     for (const file of commandsFiles) {
         const command = require(`./commands/${folder}/${file}`);
         client.commands.set(command.name, command);
     }
 }
 
-const eventsFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
-for (const file of eventsFiles) {
-    const event = require(`./events/${file}`);
-    client.on(event.name, (...args) => event.execute(...args))
+//!Event Handler
+const eventsFolders = fs.readdirSync('./events');
+for (const folder of eventsFolders) {
+    const eventsFiles = fs.readdirSync(`./events/${folder}`)
+
+    for (const file of eventsFiles) {
+        if (file.endsWith(`.js`)) {
+            const event = require(`./events/${folder}/${file}`);
+            client.on(event.name, (...args) => event.execute(...args));
+        }
+        else {
+            const eventsFiles2 = fs.readdirSync(`./events/${folder}/${file}`)
+            for (const file2 of eventsFiles2) {
+                const event = require(`./events/${folder}/${file}/${file2}`);
+                client.on(event.name, (...args) => event.execute(...args));
+            }
+        }
+    }
 }
+
+//!Commands Check
+client.on(`messageCreate`, message => {
+    const prefix = config.prefix;
+
+    if (!message.content.startsWith(prefix) || message.author.bot || !message.guild) return
+
+    const args = message.content.toLowerCase().slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command) && !client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command))) return
+
+    var comando = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command))
+
+    comando.execute(message, args);
+})
 
 client.on("messageCreate", message => {
     const prefix = "!";
