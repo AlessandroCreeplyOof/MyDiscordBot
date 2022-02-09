fe84339a24b947efa967be36d16ead3e45da8e4e
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-09 17:35:22 +0100

    Add Canvas on Ban

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -17,7 +17,7 @@ client.on("messageCreate", message => {
               var embed = new Discord.MessageEmbed()
                   .setTitle(`${utente.user.username} bannato`)
                   .setDescription(`Utente bannato da ${message.author.toString()}`)
-                  .setThumbnail(`immagini/ban.png`)
+                  .setThumbnail(`https://imgur.com/7s4EmCV`)
 
               message.channel.send({ embeds: [embed] })
           })
