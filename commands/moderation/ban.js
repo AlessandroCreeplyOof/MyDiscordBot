93eb58b43f3cdff85a01156b3a2715933e9f576e
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-10 00:13:17 +0100

    Bug Fixes

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,10 +1,22 @@
 const Discord = require("discord.js")
 
+var noperms = new Discord.MessageEmbed()
+.setTitle(`Non hai il permesso!`)
+.setDescription(`Non è stato rilevato il permesso che ti permette di eseguire la seguente azione: **BAN**`)
+.setColor(`RED`)
+.setThumbnail(`https://media.discordapp.net/attachments/941101779297378314/941101829847130122/noperms.png`)
+
+var menzione = new Discord.MessageEmbed()
+.setTitle(`Errore di sintassi`)
+.setDescription(`Non hai menzionato nessun utente da bannare!`)
+.setColor(`ORANGE`)
+.setThumbnail(`https://media.discordapp.net/attachments/941101779297378314/941101830543396864/sintassi2.png`)
+
 client.on("messageCreate", message => {
   if (message.content.startsWith("!ban")) {
       var utente = message.mentions.members.first();
       if (!message.member.permissions.has('BAN_MEMBERS')) {
-          return message.channel.send('Non hai il permesso');
+          return message.channel.send({ embeds: [noperms] });
       }
       if (!utente) {
           return message.channel.send('Non hai menzionato nessun utente');
@@ -15,9 +27,10 @@ client.on("messageCreate", message => {
       utente.ban()
           .then(() => {
               var embed = new Discord.MessageEmbed()
-                  .setTitle(`${utente.user.username} bannato`)
-                  .setDescription(`Utente bannato da ${message.author.toString()}`)
-                  .setThumbnail(`https://imgur.com/a/FAFFROJ`)
+                  .setTitle(`#BAN ${utente.user.username}`)
+                  .setDescription(`L'utente è stato bannato da: ${message.author.toString()}`)
+                  .setColor(`RED`)
+                  .setThumbnail(`https://media.discordapp.net/attachments/941101779297378314/941101829046042755/ban.png`)
 
               message.channel.send({ embeds: [embed] })
           })
