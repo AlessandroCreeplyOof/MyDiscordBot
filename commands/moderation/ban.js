c77b5f6d77c3d98a0b92ed5132c84eb8f138586c
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-16 22:02:43 +0100

    Bug Fixes

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -19,28 +19,24 @@ var nonmenzione = new Discord.MessageEmbed()
 .setThumbnail(`https://media.discordapp.net/attachments/941101779297378314/942011608996147230/giastanza-removebg-preview_1.png`)
 
 client.on("messageCreate", message => {
-  if (message.content.startsWith("!ban")) {
-      var utente = message.mentions.members.first();
-    }
-      if (!message.member.permissions.has('BAN_MEMBERS')) {
-          return message.channel.send({ embeds: [nonpermesso1] });
-      }
-      if (!utente) {
-          return message.channel.send({ embeds: [nonmenzione] });
-      }
-      if (!utente.bannable) {
-          return message.channel.send({embeds: [nonpermesso1] });
-      }
-      utente.ban()
-          .then(() => {
-              var embed = new Discord.MessageEmbed()
-                  .setTitle(`#BAN ${utente.user.username}`)
-                  .setDescription(`L'utente è stato bannato da: ${message.author.toString()}`)
-                  .addField(`Motivo: null`)
-                  .setColor(`RED`)
-                  .setThumbnail(`https://media.discordapp.net/attachments/941101779297378314/941101829046042755/ban.png`)
+    if (message.content.startsWith("!ban")) {
+        var utente = message.mentions.members.first();
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+        if (!utente.bannable) {
+            return message.channel.send('Io non ho il permesso');
+        }
+        utente.ban()
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} bannato`)
+                    .setDescription(`Utente bannato da ${message.author.toString()}`)
 
-              message.channel.send({ embeds: [embed] })
-          })
-  }
-)
+                message.channel.send({ embeds: [embed] })
+            })
+    }
+})
