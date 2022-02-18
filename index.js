e9f825ae7826b4c33e008a704b11245b3e4878f3
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-18 17:29:47 +0100

    Bug Fixes

--- index.js
+++ index.js
@@ -81,11 +81,34 @@ client.on('ready', () => {
 
     client.on("GuildMember", member => {
         if (member.user.bot) return
+
+        var annunci = new Discord.MessageButton()
+.setLabel("Canale Annunci")
+.setEmoji("📣")
+.setStyle("URL")
+.setURL("https://discord.com/channels/869222535181529140/887061611557294091")
+
+var chat = new Discord.MessageButton()
+.setLabel("Regolamento")
+.setEmoji("📜")
+.setStyle("URL")
+.setURL("https://discord.com/channels/869222535181529140/869234720049872926")
+
+var suggerimenti = new Discord.MessageButton()
+.setLabel("Suggerimenti")
+.setEmoji("🔮")
+.setStyle("URL")
+.setURL("https://discord.com/channels/869222535181529140/934745362814623814")
+
+var row = new Discord.MessageActionRow()
+.addComponents(annunci, suggerimenti, chat)
+
         var embed = new Discord.MessageEmbed()
             .setTitle("Benvenuto!")
-            .setDescription(`Ciao ${member.toString()}, benvenuto in ${member.guild.name}. Sei il **${member.guild.memberCount}° Membro**`)
+            .setDescription(`Hey ${member.toString()}, benvenuto nel server ufficiale di **Creeply!** \n \n Divertiti nel mio server e chatta con altri utenti rispettando ovviamente il regolamento \n \n **Clicca i bottoni qui sotto per capire al meglio le funzioni del server!`)
+            .setImage("https://media.discordapp.net/attachments/941101779297378314/944266920255963206/communitybanner_1.png?width=1250&height=625")
     
-        member.send({embeds: [embed]}); 
+        member.send({embeds: [embed], components: [row] }); 
     })
 
     client.on("message", message => {
