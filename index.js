5bcfc398e01f98a6dc3316151f7da8ac09c1502a
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-18 17:50:31 +0100

    Bug Fixes

--- index.js
+++ index.js
@@ -98,21 +98,8 @@ var row = new Discord.MessageActionRow()
             .setTitle("Benvenuto!")
             .setDescription(`Hey ${member.toString()}, benvenuto nel server ufficiale di **Creeply!** \n \n Divertiti nel mio server e chatta con altri utenti rispettando ovviamente il regolamento \n \n **Clicca i bottoni qui sotto per capire al meglio le funzioni del server!**`)
             .setImage("https://media.discordapp.net/attachments/941101779297378314/944266920255963206/communitybanner_1.png?width=1250&height=625")
-            .setColor("BLURPLE")
     
         member.send({embeds: [welcome], components: [row]}); 
-    })
-
-    client.on("GuildMemberRemove", member => {
-        if (member.user.bot) return
-
-        var byebye = new Discord.MessageEmbed()
-        .setTitle("Bye bye")
-        .setDescription(`Hey ${member.toString()}, ci dispiace che tu sia uscito dal server... ma se tu vorrai rientrare noi ti aspettiamo!`)
-        .setImage("https://media.discordapp.net/attachments/941101779297378314/944266920255963206/communitybanner_1.png?width=1250&height=625")
-        .setColor("BLURPLE")
-
-        member.send({embeds: [byebye]}); 
     })
 
 
