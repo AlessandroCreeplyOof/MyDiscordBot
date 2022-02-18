170efdff80e1d485f07c5b125009b8a62f3698ac
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-18 21:10:16 +0100

    Ban Module Export

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,42 +1,49 @@
 const Discord = require("discord.js")
+const { description, execute } = require("../community/suggest")
 
-var nontrovato = new Discord.MessageEmbed()
+module.exports = {
+    name: `ban`,
+    description: `Banna un utente`,
+    execute(client, messages, args) {
+
+        const nontrovato = new Discord.MessageEmbed()
 .setTitle(`Errore`)
 .setDescription(`Ooops, non sono riuscito a trovare questo utente!`)
 .setColor(`GREY`)
 .setThumbnail(`https://media.discordapp.net/attachments/941101779297378314/942011608996147230/giastanza-removebg-preview_1.png`)
 
-var nonpermesso1 = new Discord.MessageEmbed()
+const nonpermesso1 = new Discord.MessageEmbed()
 .setTitle(`Errore`)
 .setDescription(`Non hai il permesso per bannare questo utente!`)
 .setColor(`RED`)
 .setThumbnail(`https://media.discordapp.net/attachments/941101779297378314/942011608996147230/giastanza-removebg-preview_1.png`)
 
-var nonmenzione = new Discord.MessageEmbed()
+const nonmenzione = new Discord.MessageEmbed()
 .setTitle(`Errore`)
 .setDescription(`Non hai menzionato nessuno da bannare!`)
 .setColor(`GREY`)
 .setThumbnail(`https://media.discordapp.net/attachments/941101779297378314/942011608996147230/giastanza-removebg-preview_1.png`)
 
-client.on("messageCreate", message => {
-    if (message.content.startsWith("!ban")) {
-        var utente = message.mentions.members.first();
-        if (!message.member.permissions.has('BAN_MEMBERS')) {
-            return message.channel.send('Non hai il permesso');
-        }
-        if (!utente) {
-            return message.channel.send('Non hai menzionato nessun utente');
-        }
-        if (!utente.bannable) {
-            return message.channel.send('Io non ho il permesso');
-        }
-        utente.ban()
-            .then(() => {
-                var embed = new Discord.MessageEmbed()
-                    .setTitle(`${utente.user.username} bannato`)
-                    .setDescription(`Utente bannato da ${message.author.toString()}`)
-
-                message.channel.send({ embeds: [embed] })
-            })
-    }
-})
+                const utente = message.mentions.members.first();
+                if (!message.member.permissions.has('BAN_MEMBERS')) {
+                    return message.channel.send({embeds : [nonpermesso1]});
+                }
+                if (!utente) {
+                    return message.channel.send({embeds : [nonmenzione]});
+                }
+                if (!utente.bannable) {
+                    return message.channel.send({embeds : [nonpermesso1]});
+                }
+                utente.ban()
+                    .then(() => {
+                        const embed = new Discord.MessageEmbed()
+                            .setTitle(`#BAN ${utente.user.username}`)
+                            .setDescription(`L'utente ${utente.user.username} è stato bannato dal server \n 🌐 Operatore: ${message.author.toString()} \n 💬 Motivo: Non disponibile`)
+                            .setColor("ORANGE")
+                            .setThumbnail("https://media.discordapp.net/attachments/941101779297378314/941101829046042755/ban.png")
+        
+                        message.channel.send({ embeds: [embed] })
+                    })
+            }
+}
+    
--- index.js
+++ index.js
@@ -8,26 +8,33 @@ client.login("OTM3MzQ1MTk3MDYxMzEyNTIz.YfaYvg.ALjAeWYBr4eOvS4fuLyfnUR0xuM")
 
 const fs = require("fs");
 
-global.prefix = "!"
-
-//COMMANDS
 client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const file of commandFiles){
+    var command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
 const commandsFolder = fs.readdirSync("./commands");
-for (const folder of commandsFolder) {
-    const commandsFiles = fs.readdirSync(`./commands/${folder}`);
-    for (const file of commandsFiles) {
-        if (file.endsWith(".js")) {
-            const command = require(`./commands/${folder}/${file}`);
-            client.commands.set(command.name, command);
-        }
-        else {
-            const commandsFiles2 = fs.readdirSync(`./commands/${folder}/${file}`)
-            for (const file2 of commandsFiles2) {
-                const command = require(`./commands/${folder}/${file}/${file2}`);
-                client.commands.set(command.name, command);
-            }
-        }
+for(const folder of commandsFolder){
+    const commandsFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith(".js"));
+    for(const file of commandFiles){
+        const command = require(`./commands/${folder}/${file}`)
+        client.commands.set(command.name, command);
     }
 }
+
+client.on("messageCreate", message => {
+    const prefix = "!";
+
+    if(!message.content.startsWith(prefix) || message.author.bot) return
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(!client.commands.has(command)) return
+    client.commands.get(command).execute(message)
+})
 
 
