9f6aaa1464d9395f7fa0bb77d734c104a0614e52
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-18 17:45:33 +0100

    Bug Fixes

--- index.js
+++ index.js
@@ -96,8 +96,9 @@ var row = new Discord.MessageActionRow()
 
         var welcome = new Discord.MessageEmbed()
             .setTitle("Benvenuto!")
-            .setDescription(`Hey ${member.toString()}, benvenuto nel server ufficiale di **Creeply!** \n \n Divertiti nel mio server e chatta con altri utenti rispettando ovviamente il regolamento \n \n **Clicca i bottoni qui sotto per capire al meglio le funzioni del server!`)
+            .setDescription(`Hey ${member.toString()}, benvenuto nel server ufficiale di **Creeply!** \n \n Divertiti nel mio server e chatta con altri utenti rispettando ovviamente il regolamento \n \n **Clicca i bottoni qui sotto per capire al meglio le funzioni del server!**`)
             .setImage("https://media.discordapp.net/attachments/941101779297378314/944266920255963206/communitybanner_1.png?width=1250&height=625")
+            .setColor("BLURPLE")
     
         member.send({embeds: [welcome], components: [row]}); 
     })
