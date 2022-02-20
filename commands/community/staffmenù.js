fd112c801aeae957eeadc4a7c89fe1dc66c88dc2
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-20 23:57:58 +0100

    Things To Do

---
+++ commands/community/staffmenù.js
@@ -0,0 +1,12 @@
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!tdo")) {
+        var args = message.content.split(/\s+/);
+        var thingstodo = args.slice(1).join(" ")
+
+        const tdo1 = new Discord.MessageEmbed()
+        .setTitle(`💡 Things To Do`)
+        .setDescription("`", thingstodo, "` \n \n Stato: IN REVISIONE")
+        .setColor("WHITE")
+        .setTimestamp()
+    }
+})
