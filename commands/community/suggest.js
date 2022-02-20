a0fcaf945fe30859ed431c8af396da6b73ebaa87
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-20 16:56:32 +0100

    Bug Fixes

--- commands/community/suggest.js
+++ commands/community/suggest.js
@@ -3,16 +3,29 @@ const Discord = require("discord.js")
 client.on("messageCreate", message => {
     if (message.content.startsWith("!suggest")) {
         var args = message.content.split(/\s+/);
-        var suggest = args.join(" ")
+        var suggest = args.slice(1).join(" ")
+
+        var accetta = new Discord.MessageButton()
+            .setLabel("Approva")
+            .setCustomId("approvasuggest")
+            .setStyle("SUCCESS")
+
+        var rifiuta = new Discord.MessageButton()
+            .setLabel("Rifiuta")
+            .setCustomId("rifiutasuggest")
+            .setStyle("DANGER")
+
+        var row = new Discord.MessageActionRow()
+            .addComponents(accetta, rifiuta)
 
         var embed = new Discord.MessageEmbed()
         .setTitle(`💡 Nuovo Suggerimento`)
         .setThumbnail("https://media.discordapp.net/attachments/941101779297378314/944975611791822848/suggestcanvas-removebg-preview.png")
-        .setColor("AQUA")
+        .setColor("ORANGE")
         .setDescription(suggest)
         .setTimestamp()
 
-        client.channels.cache.get("944545494531715112").send({embeds: [embed]}); 
+        client.channels.cache.get("944545494531715112").send({embeds: [embed], components: [row] }); 
     }
 })
 
