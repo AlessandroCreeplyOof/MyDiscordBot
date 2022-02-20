3f80a3cb444b0cd9c5ee9d1f9b66d20c800ba5e7
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-20 15:31:27 +0100

    Bug Fixes

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js")
 client.on("messageCreate", message => {
     if (message.content == "!invite") {
         const embed = new Discord.MessageEmbed()
-            .setTitle(":woman_raising_hand: Invita i tuoi amici :man_raising_hand:")
+            .setTitle(":woman_raising_hand: Invito del server")
             .setColor("#677BC4")
-            .setDescription("Con questo invito potrai invitare i tuoi **amici** nel server \n https://discord.gg/eXWPat6CTb")
+            .setDescription("<:festa:944921599059066931> Con questo invito potrai invitare i tuoi **amici** nel server \n \n https://discord.gg/eXWPat6CTb")
         message.channel.send({embeds: [embed]})
     }
 })
