f8aae13cd159183eaab5a198b64d379f273b49f1
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-21 00:02:08 +0100

    Bug Fixes

--- commands/community/staffmenù.js
+++ commands/community/staffmenù.js
@@ -1,14 +1,1 @@
 const Discord = require("discord.js")
-
-client.on("messageCreate", message => {
-    if (message.content.startsWith("!tdo")) {
-        var args = message.content.split(/\s+/);
-        var thingstodo = args.slice(1).join(" ")
-
-        const tdo1 = new Discord.MessageEmbed()
-        .setTitle(`💡 Things To Do`)
-        .setDescription(thingstodo)
-        .setColor("WHITE")
-        .setTimestamp()
-    }
-})
