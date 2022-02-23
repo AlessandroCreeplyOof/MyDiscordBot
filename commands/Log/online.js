2878c359cc729ec03de39b37e535d24e8cf03607
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-23 17:48:18 +0100

    Bug Fixes

--- commands/Log/online.js
+++ commands/Log/online.js
@@ -1,8 +1,16 @@
 const Discord = require("discord.js")
 
+var currentdate = new Date(); 
+var datetime = "Date Time: " + currentdate.getDate() + "/"
+    + (currentdate.getMonth()+1)  + "/" 
+    + currentdate.getFullYear() + " @ "  
+    + currentdate.getHours() + ":"  
+    + currentdate.getMinutes() + ":" 
+    + currentdate.getSeconds();
+
 const online = new Discord.MessageEmbed()
 .setTitle("Bot ONLINE")
-.setDescription(":clock: Time \n `Soon...` ")
+.setDescription("⏰ Time \n ", datetime)
 .setColor("DARK_GREEN")
 
 const offline = new Discord.MessageEmbed()
