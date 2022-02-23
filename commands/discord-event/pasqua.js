7a9ca84eeda5f2dca4b0f89ae0f461070743a554
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-02-23 17:28:44 +0100

    Bug Fixes

--- commands/discord-event/pasqua.js
+++ commands/discord-event/pasqua.js
@@ -23,13 +23,66 @@ client.on("messageCreate", message => {
         var row = new Discord.MessageActionRow()
             .addComponents(apriUovo, forseUovo, chiudiUovo)
 
+            const uovomenu = new Discord.MessageActionRow()
+            .addComponents(
+                new Discord.MessageSelectMenu()
+            .setPlaceholder("Apri altre uova")
+            .setCustomId("menu")
+            .setMinValues("1")
+            .setMaxValues("1")
+            .addOptions([
+                {
+                    label: "Giorno 1",
+                    description: "Apri il primo uovo dell'evento!",
+                    value: "uovo",
+                    emoji: "<:regalo:944925534624821248>",
+                },
+                {
+                    label: "Giorno 2",
+                    description: "Apri il secondo uovo dell'evento!",
+                    value: "uovo2",
+                    emoji: "<:regalo:944925534624821248>",
+                },
+                {
+                    label: "Giorno 3",
+                    description: "Apri il terzo uovo dell'evento!",
+                    value: "uovo3",
+                    emoji: "<:regalo:944925534624821248>",
+                },
+                {
+                    label: "Giorno 4",
+                    description: "Apri il quarto uovo dell'evento!",
+                    value: "uovo4",
+                    emoji: "<:regalo:944925534624821248>",
+                },
+                {
+                    label: "Giorno 5",
+                    description: "Apri il quinto uovo dell'evento!",
+                    value: "uovo5",
+                    emoji: "<:regalo:944925534624821248>",
+                },
+                {
+                    label: "Giorno 6",
+                    description: "Apri il sesto uovo dell'evento!",
+                    value: "uovo6",
+                    emoji: "<:regalo:944925534624821248>",
+                }
+            ])
+            )
+            
+            var menu = new Discord.MessageSelectMenu()
+            .setPlaceholder("Apri altre uova")
+            .setCustomId("menu")
+            .setMinValues("1")
+            .setMaxValues("1")
+
         const embed = new Discord.MessageEmbed()
             .setTitle("Uovo di Pasqua") //Titolo
             .setColor("AQUA") // Colore principale
             .setDescription("Oooh! Un uovo di pasqua, hai trovato un uovo di pasqua, clicca il pulsante sotto stante per aprirlo! \n \n *I Regali sono completamente casuali*") //Descrizione
             .setThumbnail("https://cdn.discordapp.com/attachments/941101779297378314/942865860765298718/PasquaEgg.png") //Copertina
 
-            message.channel.send({embeds: [embed], components: [row]})
+            message.channel.send({embeds: [embed], components: [uovomenu]})
 
             const uovoaperto = new Discord.MessageEmbed()
 .setTitle("Uovo Di Pasqua | Aperto con successo")
@@ -45,56 +98,3 @@ client.on("interactionCreate", interaction => {
         }})
     }
 })
-
-const row = new Discord.MessageActionRow()
-.addComponents(
-    new Discord.MessageSelectMenu()
-.setPlaceholder("Apri altre uova")
-.setCustomId("menu")
-.setMinValues("1")
-.setMaxValues("1")
-.addOptions([
-    {
-        label: "Giorno 1",
-        description: "Apri il primo uovo dell'evento!",
-        value: "uovo",
-        emoji: "<:regalo:944925534624821248>",
-    },
-    {
-        label: "Giorno 2",
-        description: "Apri il secondo uovo dell'evento!",
-        value: "uovo2",
-        emoji: "<:regalo:944925534624821248>",
-    },
-    {
-        label: "Giorno 3",
-        description: "Apri il terzo uovo dell'evento!",
-        value: "uovo3",
-        emoji: "<:regalo:944925534624821248>",
-    },
-    {
-        label: "Giorno 4",
-        description: "Apri il quarto uovo dell'evento!",
-        value: "uovo4",
-        emoji: "<:regalo:944925534624821248>",
-    },
-    {
-        label: "Giorno 5",
-        description: "Apri il quinto uovo dell'evento!",
-        value: "uovo5",
-        emoji: "<:regalo:944925534624821248>",
-    },
-    {
-        label: "Giorno 6",
-        description: "Apri il sesto uovo dell'evento!",
-        value: "uovo6",
-        emoji: "<:regalo:944925534624821248>",
-    }
-])
-)
-
-var menu = new Discord.MessageSelectMenu()
-.setPlaceholder("Apri altre uova")
-.setCustomId("menu")
-.setMinValues("1")
-.setMaxValues("1")
