27c06a8c412f6f0a216d664e662c6297a575957b
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-01 22:04:17 +0100

    Add Battute on !battuta

--- commands/community/battute.js
+++ commands/community/battute.js
@@ -12,7 +12,17 @@ client.on("messageCreate", message => {
         .setDescription("Una tv viene gettata in acqua, ora purtroppo.. va in onda 📺")
         .setThumbnail("https://media.discordapp.net/attachments/941101779297378314/948322781718786088/tv.png")
         .setColor("ORANGE")
-    var messaggi = [embed1, embed2]
+    var embed3 = new Discord.MessageEmbed()
+    .setTitle("Battuta ORRIBILE...")
+    .setThumbnail("https://media.discordapp.net/attachments/941101779297378314/948324309540163655/astuccio.png")
+    .setDescription("L'astuccio è pericoloso... ci lasci le penne")
+    .setColor("ORANGE")
+    var embed4 = new Discord.MessageEmbed()
+    .setTitle("Battuta ORRIBILE...")
+    .setThumbnail("https://media.discordapp.net/attachments/941101779297378314/948324310035099718/termometro.png")
+    .setDescription("Oh vorrei fare una battuta sulla temperatura, ma non penso di essere in.. grado...")
+    .setColor("ORANGE")
+    var messaggi = [embed1, embed2, embed3, embed4]
     message.channel.send({ embeds: [messaggi[Math.floor(Math.random() * messaggi.length)]] });
 }
 })
