ffaf35edd0b92a341c87eee2c396785e851cb251
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-01 23:42:34 +0100

    Bug Fixes

--- commands/Log/message.js
+++ commands/Log/message.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 
 client.on("messageDelete", message => {
     
+    const LogChannel = client.channels.cache.get("948344721368027246")
     const DeletedLog = new Discord.MessageEmbed()
     .setTitle("Deleted Message")
     .addField("Deleted by", message.author, false)
@@ -9,5 +10,8 @@ client.on("messageDelete", message => {
     .addField("Content:", message.content)
     .setColor("RED")
     .setThumbnail(message.author.displayAvatarURL({dybamic: true}))
+
+    LogChannel.send({ embeds: [DeletedLog]})
+
 })
 
