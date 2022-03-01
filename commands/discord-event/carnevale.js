7e9787692aaede495871e486050b539e3b79d6a3
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-01 20:45:15 +0100

    Update carnevale.js

--- commands/discord-event/carnevale.js
+++ commands/discord-event/carnevale.js
@@ -15,8 +15,9 @@ client.on("messageCreate", message => {
         }
 
         const coriandoli = new Discord.MessageEmbed()
-        .setTitle("UOOO! CORIANDOLI!")
-        .setDescription("Hai lanciato i coriandoli in faccia ad " + utente.user.username + " ahhahaha!")
+        .setTitle("<:creeplyauguri:945362116221734922> UOOO! CORIANDOLI! 🎉")
+        .setDescription("Hai lanciato i coriandoli in faccia ad " + utente.user.username)
+        .setThumbnail("https://media.discordapp.net/attachments/941101779297378314/948304717690310797/festa-removebg-preview.png")
         .setColor("RED")
 
         message.channel.send({ embeds: [coriandoli] })
