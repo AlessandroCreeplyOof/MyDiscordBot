47adcbb3243b54618102a1b450815abaab682116
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-01 23:29:46 +0100

    Bug Fixes

--- commands/Log/message.js
+++ commands/Log/message.js
@@ -1,16 +1,5 @@
 const Discord = require("discord.js")
 
-client.on("messageCreate", message => {
-    if (member.user.bot) return
-    var embed = new Discord.MessageEmbed()
-    .setTitle(`[MESSAGE LOG] ${member.toString()}`)
-    .setDescription(`Un messaggio è stato inviato \n User: ${member.toString()} \n Messaggio: ${message.toString()}`)
-    .setTimestamp()
-    .setColor("GREEN")
-
-    client.channels.cache.get("948344721368027246").send({embeds: [embed]}); 
-})
-
 client.on("messageUpdate", message => {
     if (member.user.bot) return
     var embed = new Discord.MessageEmbed()
