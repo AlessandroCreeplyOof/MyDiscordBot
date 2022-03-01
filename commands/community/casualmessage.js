d4b1f475f80b4b9f298868e2f2d5487249369e69
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-01 23:46:03 +0100

    Update casualmessage.js

--- commands/community/casualmessage.js
+++ commands/community/casualmessage.js
@@ -3,6 +3,6 @@ const Discord = require("discord.js")
 client.on("messageCreate", message => {
     if (message.content == "ciao") {
         var messaggi = ["Ciao, come va?", "Ehi come stai?", "Tutto bene?"] 
-        message.channel.send(messaggi[Math.floor(Math.random() * messaggi.length)]);
+        message.reply(messaggi[Math.floor(Math.random() * messaggi.length)]);
     }
 })
