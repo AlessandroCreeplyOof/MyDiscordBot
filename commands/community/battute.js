54a96377c4eed62d049b8dc22c74540a552c1e9a
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-01 21:58:48 +0100

    Bug Fixes

--- commands/community/battute.js
+++ commands/community/battute.js
@@ -5,12 +5,12 @@ client.on("messageCreate", message => {
     var embed1 = new Discord.MessageEmbed()
         .setTitle("Battuta ORRIBILE...")
         .setDescription("Il gabibbo cade in un caffè... SPLASH! ☕")
-        .setThumbnail("https://media.discordapp.net/attachments/941101779297378314/948322781718786088/tv.png")
+        .setThumbnail("https://media.discordapp.net/attachments/941101779297378314/948322782079504444/caffe.png")
         .setColor("ORANGE")
     var embed2 = new Discord.MessageEmbed()
         .setTitle("Battuta ORRIBILE...")
         .setDescription("Una tv viene gettata in acqua, ora purtroppo.. va in onda 📺")
-        .setThumbnail("https://media.discordapp.net/attachments/941101779297378314/948322782079504444/caffe.png")
+        .setThumbnail("https://media.discordapp.net/attachments/941101779297378314/948322781718786088/tv.png")
         .setColor("ORANGE")
     var messaggi = [embed1, embed2]
     message.channel.send({ embeds: [messaggi[Math.floor(Math.random() * messaggi.length)]] });
