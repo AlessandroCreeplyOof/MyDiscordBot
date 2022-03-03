eb9050d36fd823b2e8ae8a83f42bb82ee8c208be
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-03 23:25:24 +0100

    Bug Fixes

---
+++ commands/Log/role.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js")
+
+client.on("roleCreate", role => {
+
+    const ruolocreato = new Discord.MessageEmbed()
+    .setTitle("[ROLE CREATED]")
+    .setDescription("Added Role to the server.")
+    .addField("User:", `${role.author.ToString()}`, false)
+    .addField("Role:", `${role}`, false)
+    .setTimestamp()
+    .setColor("PURPLE")
+
+    client.channels.cache.get("949068152048861214").send({embeds: [ruolocreato]})
+})
+
+client.on("roleDelete", role => {
+
+    const ruolodelete = new Discord.MessageEmbed()
+    .setTitle("[ROLE DELETED]")
+    .setDescription("Removed Role to the server.")
+    .addField("User:", `${role.author.ToString()}`, false)
+    .addField("Role:", `${role}`, false)
+    .setTimestamp()
+    .setColor("PURPLE")
+
+    client.channels.cache.get("949068152048861214").send({embeds: [ruolodelete]})
+})
+
+client.on("roleUpdate", (oldRole, newRole) => {
+
+    const ruolodelete = new Discord.MessageEmbed()
+    .setTitle("[ROLE DELETED]")
+    .setDescription("Removed Role to the server.")
+    .addField("User:", `${newRole.author.ToString()}`, false)
+    .addField("New Role:", `${newRole}`, false)
+    .addField("Old Role:", `${oldRole}`, false)
+    .setTimestamp()
+    .setColor("PURPLE")
+
+    client.channels.cache.get("949068152048861214").send({embeds: [ruolodelete]})
+})
