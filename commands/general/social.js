dbceee0838e4f1e8c0f4e7aa437bfe5e8d91426a
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-03 21:10:13 +0100

    Fixes !social

--- commands/general/social.js
+++ commands/general/social.js
@@ -8,7 +8,8 @@ client.on("messageCreate", message => {
         .setDescription("Ecco alcuni link utili! \n 🎥 **YouTube** \n - https://youtube.com/c/Creeply \n <:githublogo:947452305538691093> **GitHub** \n - https://github.com/AlessandroCreeplyOof")
         .setFooter("Comando in continuo aggiornamento...")
         .setTimestamp()
+        .setImage("https://media.discordapp.net/attachments/941101779297378314/944266920255963206/communitybanner_1.png?width=1342&height=671")
 
-        message.channels.send({ embeds: social })
+        message.channel.send({ embeds: [social] })
     }
 })
