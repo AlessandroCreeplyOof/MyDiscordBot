bb79a7bc9f35ceb78fdba62b31ef6b4fe71bd950
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-04 17:14:38 +0100

    Channels Log

--- commands/Log/channel.js
+++ commands/Log/channel.js
@@ -5,8 +5,26 @@ client.on("channelCreate", channel => {
     const canalecreato = new Discord.MessageEmbed()
     .setTitle("[CREATED CHANNEL]")
     .setDescription("Created the channel")
-    .addField(`**Type**`,`${channel.type}`) 
-    .setFooter(`**ChannelID:** ${channel.id} | **CreatedAT:** ${channel.createdAt}`)
+    .addField(`**Type:**`,`${channel.type}`) 
+    .addField(`**ChannelName:**`, `${channel.name}`)
+    .addField(`**ChannelID:**`, `${channel.id}`)
+    .addField(`**CreatedAT:**`, `${channel.createdAt}`)
+    .setColor("PURPLE")
+    .setTimestamp()
 
     client.channels.cache.get("937711348664635453").send( { embeds: [canalecreato] } )
+})
+
+client.on("channelDelete", channel => {
+
+    const canaledelete = new Discord.MessageEmbed()
+    .setTitle("[DELETED CHANNEL]")
+    .setDescription("Deleted the channel")
+    .addField(`**Type:**`,`${channel.type}`) 
+    .addField(`**ChannelName:**`, `${channel.name}`)
+    .addField(`**ChannelID:**`, `${channel.id}`)
+    .setColor("PURPLE")
+    .setTimestamp()
+
+    client.channels.cache.get("937711348664635453").send( { embeds: [canaledelete] } )
 })
