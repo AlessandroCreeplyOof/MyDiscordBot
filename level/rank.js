da7f77817f4018cb588f51084c556dbc1eb4351a
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-04 19:59:18 +0100

    Leveling System

--- level/rank.js
+++ level/rank.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js")
 
 client.on("messageCreate", message => {
-    if(message.content == "!rank") {
+    if (message.content.startsWith("!avatar")) {
 
         const rankcard = new Discord.MessageEmbed()
         .setTitle(`Ranking - ${message.author.ToString()}`)
         .setDescription("🔥 Livello 1 \n ◻️ ◽◽◽◽◽ \n XP 0/100")
-        .setThumbnail(message.author.user.displayAvatarURL())
         .setFooter("Il Sistema di livellamento è al momento disattivato!")
 
         message.channel.send({ embeds: [rankcard] })
