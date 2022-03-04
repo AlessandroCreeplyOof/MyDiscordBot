58588ba33c96fab7063abd28e83508e3c820fe0f
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-04 17:26:24 +0100

    Update channel.js

--- commands/Log/channel.js
+++ commands/Log/channel.js
@@ -16,15 +16,32 @@ client.on("channelCreate", channel => {
 })
 
 client.on("channelDelete", channel => {
+    let channelDeleteAuthor = channelDelete.action.author
 
     const canaledelete = new Discord.MessageEmbed()
     .setTitle("[DELETED CHANNEL]")
     .setDescription("Deleted the channel")
-    .addField(`**Type:**`,`${channel.type}`) 
     .addField(`**ChannelName:**`, `${channel.name}`)
-    .addField(`**ChannelID:**`, `${channel.id}`)
+    .addField(`**Author**`, `${channelDeleteAuthor}`)
     .setColor("PURPLE")
     .setTimestamp()
 
     client.channels.cache.get("937711348664635453").send( { embeds: [canaledelete] } )
+})
+
+client.on("channelUpdate", channel => {
+    let channelDeleteAuthor = channelDelete.action.author
+
+    const canaleupdate = new Discord.MessageEmbed()
+    .setDescription("Channel name updated.")
+    .addField("Old channel name", `\`${oldChannel.name}\``, true)
+    .addBlankField(true)
+    .addField("New channel name", `\`${newChannel.name}\``, true)
+    .addBlankField(true)
+    .addField("Channel category", `${newCategory}`, true)
+    .setFooter(`Channel ID: ${newChannel.id} 🔥`)
+    .setTimestamp()
+    .setColor("PURPLE")
+
+    client.channels.cache.get("937711348664635453").send( { embeds: [canaleupdate] } )
 })
