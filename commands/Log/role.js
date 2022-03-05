f77bed31634ac9a3b789c233e2aae43a1aaea09b
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-05 13:06:45 +0100

    Update role.js

--- commands/Log/role.js
+++ commands/Log/role.js
@@ -1,36 +1,12 @@
 const Discord = require("discord.js")
 
-client.on("roleCreate", role => {
-
-    const ruolocreato = new Discord.MessageEmbed()
-    .setTitle("[ROLE CREATED]")
-    .setDescription("Added Role to the server.")
-    .addField("Role:", `${role.name}`, false)
-    .setTimestamp()
-    .setColor("PURPLE")
-
-    client.channels.cache.get("949068152048861214").send({embeds: [ruolocreato]})
-})
-
-client.on("roleDelete", role => {
-
-    const ruolodelete = new Discord.MessageEmbed()
-    .setTitle("[ROLE DELETED]")
-    .setDescription("Removed Role to the server.")
-    .addField("Role:", `${role.name}`, false)
-    .setTimestamp()
-    .setColor("PURPLE")
-
-    client.channels.cache.get("949068152048861214").send({embeds: [ruolodelete]})
-})
-
 client.on("roleUpdate", (oldRole, newRole) => {
 
     const ruolodelete = new Discord.MessageEmbed()
     .setTitle("[ROLE DELETED]")
     .setDescription("Removed Role to the server.")
-    .addField("New Role:", `${newRole}`, false)
-    .addField("Old Role:", `${oldRole}`, false)
+    .addField("New Role:", `${newRole.name}`, false)
+    .addField("Old Role:", `${oldRole.name}`, false)
     .setTimestamp()
     .setColor("PURPLE")
 
