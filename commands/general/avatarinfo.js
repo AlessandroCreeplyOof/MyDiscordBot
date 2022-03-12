db2d693d8594f7bff0d9958d69f96c079f2d6cc1
AlessandroCreeplyOof <98062423+AlessandroCreeplyOof@users.noreply.github.com>
2022-03-12 12:18:11 +0100

    Update avatarinfo.js

--- commands/general/avatarinfo.js
+++ commands/general/avatarinfo.js
@@ -13,5 +13,22 @@ module.exports = {
                 required: true
             }
         ]
+    },
+    
+    execute(interaction) {
+
+        var avataruser = interaction.options.getUser("user")
+        let utente = interaction.guild.members.cache.get(avataruser.id)
+
+        let embed = new Discord.MessageEmbed()
+        .setTitle(utente.user.tag)
+        .setDescription("L'avatar di questo utente")
+        .setImage(utente.user.displayAvatarURL({
+            dynamic: true,
+            format: "png",
+            size: 512
+        }))
+
+        interaction.reply({ embeds: [embed] })
     }
 }
